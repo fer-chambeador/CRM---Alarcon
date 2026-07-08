0ea5a56d362df1eecc788966a7e868e99cf8af41
Fer Alarcon <fer@chambas.ai>
2026-07-08 14:08:54 -0600

    wa-bridge: Dockerfile para Railway

--- wa-bridge/server.js
+++ wa-bridge/server.js
@@ -33,6 +33,7 @@ const client = new Client({
   authStrategy: new LocalAuth({ dataPath: './session' }),
   puppeteer: {
     headless: true,
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
   },
 })
